fd6730fc1cec8c7ecf26194bcbb23e8996422a01
Juan Brizuela <juanbrizuelago@gmail.com>
2023-04-19 01:21:45 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# ME GPT with OPEN AI API AND ELEVENLABS API
+
+## 🔳 Introduction
+
+This is a simple example of how to use the Open AI API and the ElevenLabs API to generate speech to speech and text with the text-davinci-003 model. The UI/UX was bulit with [Gradio](https://gradio.app/).
+
+## 📄 Requirements
+
+- Python 3.6 or higher
+- pip3
+- virtualenv
+
+## 📦 Installation
+
+### 🐍 Python
+
+```bash
+$ virtualenv -p python venv
+$ source venv/bin/activate
+```
+
+### 📚 Libraries
+
+```bash
+$ pip install -r requirements.txt
+```
+
+## 🚀 Usage
+
+```bash
+$ python app.py
+```
+
+## 🔑 Get your Open AI and ElevenLabs API keys here and start clonig your voice!
+
+- [Open AI](https://platform.openai.com/)
+- [ElevenLabs](https://beta.elevenlabs.io/)
+
+## ℹ️ Note
+
+In order to clone your voice, you'll need to upgrade to at least the Starter plan on ElevenLabs. The free plan only allows you to use default voices. You can find more information about the pricing [here](https://beta.elevenlabs.io/pricing).
---
+++ app.py
@@ -0,0 +1,65 @@
+from elevenlabslib import ElevenLabsUser
+from dotenv import load_dotenv
+import gradio as gr
+import openai
+import winsound
+from elevenlabslib import *
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+import os
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("ELEVENLABS_API_KEY")
+user = ElevenLabsUser(api_key)
+
+messages = [
+    "You're name is Juan Eduardo Brizuela. You're 23 years old. You can speak multiple languages. "]
+
+preamble = "You're name is Juan Eduardo Brizuela. You're 23 years old. You can speak multiple languages."
+
+
+def transcribe(audio):
+    global messages
+
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+    messages.append(f"\n{transcript['text']}")
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{preamble}{messages[-1]}",
+        max_tokens=80,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    system_message = response["choices"][0]["text"]
+    messages.append(f"{system_message}")
+
+    voice = user.get_voices_by_name("Brizuela")[0]
+    audio = voice.generate_audio_bytes(system_message)
+
+    audio = AudioSegment.from_file(io.BytesIO(audio), format="mp3")
+    audio.export("output.wav", format="wav")
+
+    winsound.PlaySound("output.wav", winsound.SND_FILENAME)
+
+    chat_transcript = "\n".join(messages)
+    return chat_transcript
+
+
+iface = gr.Interface(
+    fn=transcribe,
+    inputs=gr.Audio(source="microphone", type="filepath",
+                    placeholder="Please, start speaking. Remember to stop the recording once you're done..."),
+    outputs="text",
+    title="🤖 ME GPT 🤖",
+    description="🌟 Please ask me your question and I will respond both verbally and in text to you...",
+)
+
+iface.launch()
